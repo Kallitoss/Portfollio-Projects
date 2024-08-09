08aab387acc5556f16c60b800dc62a8e653944ec
Kallitoss <153299715+Kallitoss@users.noreply.github.com>
2024-08-09 14:45:39 +0200

    Covid 19 Analysis-Portfolio Project.sql

--- Covid 19 Analysis-Portfolio Project.sql
+++ Covid 19 Analysis-Portfolio Project.sql
@@ -16,7 +16,7 @@ FROM COVIDDEATHS
 ORDER BY 1,2 
 
 
---lookin at total casses v total deaths
+--looking at total casses v total deaths
 --Shows likelihood of dying if infected by COVID
 
 SELECT location, date, total_cases, total_deaths, (total_deaths/nullif(total_cases,0)*100)  AS DeathProcentage
@@ -25,14 +25,14 @@ WHERE continent IS NOT NULL
 ORDER BY 1,2
 
 
---lookin at total casses v total deaths in my country
+--looking at total casses vs total deaths in my country
 
 SELECT location, date, total_cases, total_deaths, (total_deaths/NULLIF(total_cases, 0) *100) AS DeathProcentage
 FROM COVIDDEATHS
 WHERE location LIKE '%erbia%'
 ORDER BY 1,2
 
---looking at total casses v total deaths in all countrys whose names start on letter S
+--looking at total casses vs total deaths in all countrys whose names start on letter S
 
 SELECT location, date, total_cases, total_deaths, (total_deaths/NULLIF(total_cases, 0) *100) AS DeathProcentage
 FROM COVIDDEATHS
@@ -59,7 +59,7 @@ GROUP BY location, population
 ORDER BY ProcentageOfPeopleThatGotCovid DESC
 
 
---Showing countries with highest death rate PER POPULATION
+--Showing countries with the highest death rate PER POPULATION
 
 SELECT location, MAX(total_deaths) AS TotalDeathCount
 FROM COVIDDEATHS
@@ -70,7 +70,7 @@ ORDER BY TotalDeathCount DESC
 
 --LET'S BREAK THINGS DOWN BY CONTINET
 
---Here i will show continents with the highest death count per population
+--Here I will show the continents with the highest death count per population
 
 SELECT continent, MAX(total_deaths) AS TotalDeathCount
 FROM COVIDDEATHS
@@ -81,7 +81,7 @@ ORDER BY TotalDeathCount DESC
 
 
 ------Looking at total population vs vaccinations
--- Showing percentage of population that recieved at least one covid vaccine
+-- Showing the percentage of the population that received at least one COVID vaccine
 
 SELECT DEATHS.continent, DEATHS.location, DEATHS.date, DEATHS.population, 
 VAX.new_vaccinations, SUM(CONVERT(float,VAX.new_vaccinations)) OVER (PARTITION BY DEATHS.location ORDER BY DEATHS.location , DEATHS.date) AS RollingPoepleVaccinated
@@ -93,7 +93,7 @@ WHERE DEATHS.continent IS NOT NULL
 ORDER BY 2, 3 
 
 
---Using CTE to perform calculation on partition by COLUMN that was created in previous query
+--Using CTE to perform a calculation on partition by COLUMN that was created in the previous query
 
 WITH PopvsVax (Continent, Location, Date, population, new_vaccinations, RollingPoepleVaccinated)
 AS
@@ -111,7 +111,7 @@ SELECT * , (RollingPoepleVaccinated/population) *100
 FROM PopvsVax 
 
 
---- Using Temp table to perform calculation on partition by COLUMN that was created in previous query
+--- Using Temp table to perform a calculation on partition by COLUMN that was created in the previous query
 DROP TABLE IF EXISTS
 CREATE TABLE #PercentPopulationVaccinated
 (Continent nvarchar(255),
@@ -136,7 +136,7 @@ WHERE DEATHS.continent IS NOT NULL
 SELECT * , (RollingPoepleVaccinated/population) *100
 FROM #PercentPopulationVaccinated
 
---Creating view to store data for future visualizations 
+--Creating a view to store data for future visualizations 
 
 CREATE VIEW PercentPopulationVaccinatedD AS
  SELECT DEATHS.continent, DEATHS.location, DEATHS.date, DEATHS.population, 
